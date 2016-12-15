2687548d5ecf6dbce259d281300e7765ece8c39f
Lahiru Jayathilake <lahirujayathilake@gmail.com>
2016-12-15 17:01:40 +0530

    change the structure of the er diagram

--- ERD/Queries.php
+++ ERD/Queries.php
@@ -134,11 +134,12 @@ $stmt->bindParam(':studentId', $studentId);
 $stmt->bindParam(':classId', $classId);
 
 #(10)
-SELECT DATE_FORMAT(SA.attend_date, "%d-%b-%Y"), SA.is_late, CM.module_code, CM.class_type, TA.student_id, CONCAT(ST.std_first_name , ' ', ST.std_last_name) student_name 
+SELECT DATE_FORMAT(SA.class_date, "%d-%b-%Y"), SA.is_late, SA.is_present, CM.module_code, CM.class_type, TA.student_id, CONCAT(ST.std_first_name , ' ', ST.std_last_name) student_name 
 FROM student_attendance SA 
 NATURAL JOIN takes TA 
 NATURAL JOIN class_module CM
 LEFT JOIN student ST 
 ON ST.student_id = TA.student_id 
-WHERE CM.module_code LIKE "%" OR CM.class_type LIKE "%" OR CONCAT(ST.std_first_name , ' ', ST.std_last_name) LIKE "%" OR DATE_FORMAT(SA.attend_date, "%d-%b-%Y") LIKE "%"
+WHERE CM.module_code LIKE "%" OR CM.class_type LIKE "%" OR CONCAT(ST.std_first_name , ' ', ST.std_last_name) LIKE "%" OR DATE_FORMAT(SA.class_date, "%d-%b-%Y") LIKE "%"
+
 
